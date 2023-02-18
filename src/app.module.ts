284dd4e622a68ddfcc18b49fad54dbd6f9360307
KuzyaCat <lizerd34@mail.ru>
2023-02-18 20:18:08 +0300

    Add database, config and environment modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { DatabaseModule } from './database/database.module';
+import { EnvironmentModule } from './environment/environment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    DatabaseModule,
+    EnvironmentModule,
+  ],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/environment/environment.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryColumn, Column } from 'typeorm';
+import { ObjectType, Field } from '@nestjs/graphql';
+
+import { TABLE_NAMES } from '../config/constants/table-names';
+
+@Entity({ name: TABLE_NAMES.ENVIRONMENTS })
+@ObjectType()
+export class Environment {
+  @PrimaryColumn()
+  @Field()
+  name: string;
+
+  @Column()
+  @Field()
+  withCountries: boolean = true;
+
+  @Column()
+  @Field()
+  withTeams: boolean = true;
+
+  @Column()
+  @Field()
+  deleted: boolean = false;
+}
